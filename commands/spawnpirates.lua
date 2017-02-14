feb6d1497b841deaf605c5655a13634f3ef34711
Douglasg14b <douglasg14b@gmail.com>
2017-02-13 19:08:16 -0800

    Adds spawning pirates on designated player

--- commands/spawnpirates.lua
+++ commands/spawnpirates.lua
@@ -1,9 +1,28 @@
 -- Copyright 2017 Douglas Gaskell Apache License 2.0
 -- v1.0
 
-function execute(sender, commandName, attackers)
-   	Player(sender):addScriptOnce("cmd/spawnpirates.lua", attackers)
+function execute(sender, commandName, difficulty, playerName)
+    if playerName ~= nil then --Attempt to find player
+        local player = findPlayer(playerName)
+        if player ~= false then -- Player found, spawn on player
+            player:addScriptOnce("cmd/spawnpirates.lua", difficulty)
+        else
+            Player(sender):sendChatMessage("SpawnPirates", 1, "Player '" .. playerName .. "' is not online or does not exist.")
+        end
+    else -- No player provided, spawn on sender
+   	    Player(sender):addScriptOnce("cmd/spawnpirates.lua", difficulty)
+    end
     return 0, "", ""
+end
+
+function findPlayer(playerName)
+    for i, player in pairs({Server():getOnlinePlayers()}) do
+        if player.name:lower() == playerName:lower() then
+          print("found player")
+          return player
+        end
+    end
+    return false
 end
 
 function getDescription()
@@ -11,5 +30,7 @@ function getDescription()
 end
 
 function getHelp()
-    return "Echoes all given parameters to console. Usage: /echo This is a test"
+    return "Spawns a pirate attack with the provided difficulty." ..
+            "/spawnpirates {difficulty}" ..
+           " Difficulties: 1-11"
 end
